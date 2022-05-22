d03bc73867404a11efa4c46bb86832147006fc0a
Saad Adel <saad.adel539@yahoo.com>
2022-05-22 17:30:21 +0200

    user UserProfile class in update user instead of api.Record

--- backend/api/user/router.py
+++ backend/api/user/router.py
@@ -2,7 +2,7 @@
 
 from fastapi import APIRouter, Body, status, Depends
 
-import models.api as api 
+import models.api as api
 import models.core as core
 import utils.regex as regex
 import utils.db as db
@@ -13,44 +13,45 @@ from typing import Any
 
 router = APIRouter()
 
+
 class UserProfile(BaseModel):
-    password : str | None = None
-    display_name : str | None = None
-    email : str | None = None
+    password: str | None = None
+    display_name: str | None = None
+    email: str | None = None
 
 
 @router.post("/create/{shortname}", response_model=api.Response, response_model_exclude_none=True)
-async def create_user(shortname : str, profile : UserProfile, invitation : str) -> api.Response:
+async def create_user(shortname: str, profile: UserProfile, invitation: str) -> api.Response:
     """ Register a new user by invitation """
-    if not invitation: 
+    if not invitation:
         # TBD validate invitation.
         # jwt-signed shortname, email and expiration time
         raise api.Exception(status_code=400, error=api.Error(type="create", code=50, message="bad invitation"))
     if not profile.password:
         raise api.Exception(status_code=400, error=api.Error(type="create", code=50, message="empty password"))
 
-    user = core.User (owner_shortname=shortname, shortname=shortname, password=profile.password)
+    user = core.User(owner_shortname=shortname, shortname=shortname, password=profile.password)
 
     if profile.display_name:
-        user.display_name=profile.display_name
+        user.display_name = profile.display_name
 
     if profile.email:
-        user.email=profile.email
+        user.email = profile.email
 
     db.create("users", user)
     return api.Response(status=api.Status.success)
 
 
 @router.get("/profile", response_model=api.Response, response_model_exclude_none=True)
-async def get_profile( shortname=Depends(JWTBearer())) -> api.Response:
+async def get_profile(shortname=Depends(JWTBearer())) -> api.Response:
     user = db.load("users", shortname, core.User)
-    attributes : dict[str,Any] = { }
+    attributes: dict[str, Any] = {}
     if user.email:
         attributes["email"] = user.email
     if user.display_name:
         attributes["display_name"] = user.display_name
     record = api.Record(
-        subpath= "users",
+        subpath="users",
         shortname=user.shortname,
         resource_type=core.ResourceType.user,
         attributes=attributes
@@ -59,35 +60,36 @@ async def get_profile( shortname=Depends(JWTBearer())) -> api.Response:
 
 
 @router.post("/profile", response_model=api.Response, response_model_exclude_none=True)
-async def update_profile( profile : api.Record, shortname=Depends(JWTBearer())
-) -> api.Response:
+async def update_profile(profile: UserProfile, shortname=Depends(JWTBearer())
+                         ) -> api.Response:
     """Update user profile"""
     user = db.load("users", shortname, core.User)
 
-    if "password" in profile.attributes:
-        user.password = profile.attributes["password"]
+    if profile.password:
+        user.password = profile.password
 
-    if "display_name" in profile.attributes:
-        user.display_name=profile.attributes["display_name"]
+    if profile.display_name:
+        user.display_name = profile.display_name
 
-    if "email" in profile.attributes:
-        user.email=profile.attributes["email"]
+    if profile.email:
+        user.email = profile.email
 
     db.update("users", user)
     return api.Response(status=api.Status.success)
 
 
-@router.post( "/login", response_model=api.Response, response_model_exclude_none=True,)
+@router.post("/login", response_model=api.Response, response_model_exclude_none=True,)
 async def login(
-    shortname: str = Body(...), #, regex=regex.USERNAME),
-    password: str = Body(...), #, regex=regex.PASSWORD),
+    shortname: str = Body(...),  # , regex=regex.USERNAME),
+    password: str = Body(...),  # , regex=regex.PASSWORD),
 ) -> api.Response:
     """Login and generate refresh token"""
     user = db.load("users", shortname, core.User)
-    if user and user.password == password: 
+    if user and user.password == password:
         access_token = sign_jwt({"username": shortname})
-        return api.Response(status=api.Status.success, auth_token = access_token )
+        return api.Response(status=api.Status.success, auth_token=access_token)
     raise api.Exception(status.HTTP_401_UNAUTHORIZED, api.Error(type="auth", code=10, message="Bad creds"))
+
 
 @router.post("/delete", response_model=api.Response, response_model_exclude_none=True)
 async def delete(shortname=Depends(JWTBearer())) -> api.Response:
