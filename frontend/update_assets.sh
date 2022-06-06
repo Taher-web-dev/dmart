1ef919270132e769a1ef517bd49486970b3220f3
Kefah Issa <kefah@imx.sh>
2022-06-06 07:38:07 +0300

    updating gitignore

--- frontend/update_assets.sh
+++ frontend/update_assets.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-assets/bootstrap-icons.css
-assets/bootstrap.min.css
-assets/bootstrap.min.css.map
-assets/bootstrap.rtl.min.css
-assets/bootstrap.rtl.min.css.map
-assets/fonts/bootstrap-icons.woff
-assets/fonts/bootstrap-icons.woff2
+cp ./node_modules/bootstrap/dist/css/bootstrap.min.css* ./assets/
+ls -alh assets/bootstrap.min.css assets/bootstrap.min.css.map
+cp ./node_modules/bootstrap/dist/css/bootstrap.rtl.min.css* ./assets/ 
+ls -alh assets/bootstrap.rtl.min.css assets/bootstrap.rtl.min.css.map
+cp ./node_modules/bootstrap-icons/font/bootstrap-icons.css ./assets/
+ls -alh assets/bootstrap-icons.css
+cp ./node_modules/bootstrap-icons/font/fonts/bootstrap-icons.woff* ./assets/fonts/
+ls -alh assets/fonts/bootstrap-icons.woff assets/fonts/bootstrap-icons.woff2
