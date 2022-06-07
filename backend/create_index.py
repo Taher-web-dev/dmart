2d1d90604bb48093499a766fabffc9393fdea686
Saad Adel <saad.adel539@yahoo.com>
2022-06-07 22:20:11 +0200

    remove redis related changes (pushed from another branch)

--- backend/create_index.py
+++ backend/create_index.py
@@ -6,36 +6,25 @@ import sys
 
 import json
 
-space_name = "products"
-search.create_index("products")
-subpath = "content"
+search.create_index()
+subpath = "myposts"
 total, locators = db.locators_query(
-    api.Query(space_name=space_name, subpath=subpath, type=api.QueryType.subpath, limit=10000)
+    api.Query(subpath=subpath, type=api.QueryType.subpath)
 )
 
 for one in locators:
     myclass = getattr(sys.modules["models.core"], core.ResourceType("content").title())
     # myclass = db.resource_class(core.ResourceType(one.__class__.__name__.lower()))
-    meta = db.load(space_name=space_name, subpath=one.subpath, shortname=one.shortname, class_type=myclass)
-    search.save_entry(space_name=space_name, subpath=subpath, meta=meta)
+    meta = db.load(one.subpath, one.shortname, myclass)
+    search.save_meta(subpath, meta)
 
-# TEST SEARCH FUNCTION
-# ret = search.search(
-#     space_name=space_name, 
-#     search="", 
-#     filters={"filter_shortnames": ["5028", "5067", "5071"], "filter_types": ["content", "media"]},
-#     limit=15, 
-#     offset=0,
-#     include=["uuid", "shortname"],
-#     sort_by="shortname"
-# )
-# print("Number of stored documents : ", len(ret))
-# for item in ret:
-#     print("\nRET: ", ret)
+ret = search.index.search("alibaba")
+print("Returned : ", len(ret.docs))
+if ret.docs:
+    for one in ret.docs:
+        # print(json.dumps(json.loads(json.loads(one.json)), indent=4))
+        print(json.dumps(json.loads(one.json), indent=4))
 
-
-
-print(f"Added the content inside: {subpath}, to redis DB")
 
 # ret = search.index.search(Query("curl")) # .return_field("$.meta.is_active", as_field="is_active")).docs
 # print(ret.total)
